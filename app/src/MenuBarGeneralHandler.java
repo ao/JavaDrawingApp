dffd4d34a70d74be1c91eabafc59817d14c01715
Andrew Odendaal <andrew@hsmoore.com>
2017-05-15 01:23:56 +0100

    Add new menubar items

--- app/src/MenuBarGeneralHandler.java
+++ app/src/MenuBarGeneralHandler.java
@@ -2,13 +2,37 @@ import javax.swing.*;
 import java.awt.event.*;
 
 public class MenuBarGeneralHandler extends WindowAdapter implements ActionListener {
+
+    public DrawingApp daInstance;
+
+    public MenuBarGeneralHandler(DrawingApp daInstance) {
+        this.daInstance = daInstance;
+    }
     public void actionPerformed(ActionEvent e) {
-        if(e.getActionCommand().equals("Exit")) {
+        if(e.getActionCommand().equals("New")) {
+
+            daInstance.createFrame();
+
+        } else if(e.getActionCommand().equals("Exit")) {
+
             System.exit(0);
+
+        } else if(e.getActionCommand().equals("Open")) {
+
+            //open a file
+
+        } else if(e.getActionCommand().equals("Save")) {
+
+            //save a file
+
         } else if(e.getActionCommand().equals("About")) {
+
             JOptionPane.showMessageDialog(null, "Java Drawing App", "About", JOptionPane.PLAIN_MESSAGE);
+
         } else {
+
             //e.getActionCommand()
+
         }
     }
 }
